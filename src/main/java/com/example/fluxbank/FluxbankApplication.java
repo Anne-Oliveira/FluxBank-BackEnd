55209099cc354a08ef40efdf54c5fe9bdcd4682a
Anne de Andrade Oliveira <a.andrade.oliveira@avanade.com>
2025-12-08 09:25:31 -0300

    Subindo projeto back-end

---
+++ src/main/java/com/example/fluxbank/FluxbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxbankApplication.class, args);
+	}
+
+}
